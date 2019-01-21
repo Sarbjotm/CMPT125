2a7a5c65e2cfb22ad729128a65e1f63b182c6cb3
Sarbjot Mann <sarbjotmann@Sarbjots-MacBook-Pro.local>
2019-01-20 17:18:04 -0800

    Assignment 2 Q4

---
+++ Calorie.c
@@ -0,0 +1,60 @@
+//Pyramid
+#include <stdio.h>
+
+
+
+int main(){
+    printf("Welcome user, please select a beverage by entering its number \n");
+    printf("Your choices are as follows \n");
+    printf("\t0:Coke\n");
+    printf("\t1:Root Beer\n");
+    printf("\t2:Dr.Pepper\n");
+    printf("\t3:Sprite\n");
+    printf("\t4:Pepsi\n");
+    int tries = 3;
+    int number;
+    
+    while (tries != 0){
+        printf("\n\nPlease make a selection: ");
+        scanf("%d", &number);
+        if(number == 0){
+            printf("Selected Drink: Coke \n");
+            printf("Calorie Count: 140 Calories \n");
+            break;
+        }
+        else if(number == 1){
+            printf("Selected Drink: Root Beer \n");
+            printf("Calorie Count: 41 Calories \n");
+            break;
+        }
+        
+        else if(number == 2){
+            printf("Selected Drink: Dr. Pepper \n");
+            printf("Calorie Count: 150 Calories \n");
+            break;
+        }
+        else if(number == 3){
+            printf("Selected Drink: Sprite \n");
+            printf("Calorie Count: 39 Calories \n");
+            break;
+        }
+        else if(number == 4){
+            printf("Selected Drink: Pepsi \n");
+            printf("Calorie Count: 150 Calories \n");
+            break;
+        }
+        else{
+            tries--;
+            if (tries == 0){
+                printf("Invalid \n");
+                printf("%d tries left \n", tries);
+            }
+            else{
+                printf("Invalid, try again \n");
+                printf("You have %d tries left", tries);
+            }
+        }
+        
+    }
+    
+}
