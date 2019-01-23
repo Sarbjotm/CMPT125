da1bcc8d7091ba2f075c857c67165dba488d4ee1
Sarbjot Mann <sarbjotmann@d207-023-192-168.wireless.sfu.ca>
2019-01-23 15:50:40 -0800

    Sorting Algorithm

---
+++ Sorting.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(){
+    int array[10] = {5,4,1,9,7,2,19, 20, 30, 1};
+    int length = (sizeof(array) / sizeof(array[0]));
+    int tmp;
+    for (int i = 0; i < length; i++){
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+    for (int i = 0; i < length; i++){
+        if(i == 9){
+            break;
+        }
+        else{
+        for(int j = i+1; j <= 9; j++){
+            if (array[j] <= array[i]){
+                tmp = array[i];
+                array[i] = array[j];
+                array[j] = tmp;
+            }
+            }
+        }
+    }
+    for (int i = 0; i < length; i++){
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+    
+    
+    
+}
