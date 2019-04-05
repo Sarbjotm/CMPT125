c1e88cd2a4ad502799f791f5a08d3daf2f96356d
Sarbjot <sarbjotm@sfu.ca>
2019-04-04 17:37:51 -0700

    Pushing comparing 2 strings

---
+++ Final/Compare2Strings.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+/*
+ If 2 strings are the same word then we will return 1 otherwise return 0
+ */
+
+
+int same_string(char string1[], char string2[]){
+    int i = 0;
+    int result = 1;
+    while (string1[i] != '\0'){
+        if(string1[i] == string2[i]){
+            i = i+1;
+        }
+        else{
+            result = 0;
+            break;
+        }
+    //This won't check lengths so lets check if string1 has extra characters or not.
+    }
+    if(string1[i] == '\0' && string2[i] != '\0'){
+        return 0;
+    }
+    return result;
+    
+}
+
+
+int main(){
+    char string1[] = "Hello";
+    char string2[] = "";
+    int similiar = same_string(string1, string2);
+    if (similiar == 1){
+        printf("The strings are the same \n");
+    }
+    else{
+        printf("Not the same \n");
+    }
+    
+    
+    return 0;
+}
